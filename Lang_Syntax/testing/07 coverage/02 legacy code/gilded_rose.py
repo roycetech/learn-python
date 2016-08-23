71fb3c224cdbf2dab6175591d34dc2760ea85f3b
Royce Remulla <royce.com@gmail.com>
2016-08-23 15:25:43 +0800

    Completed course

--- Lang_Syntax/testing/07 coverage/02 legacy code/gilded_rose.py
+++ Lang_Syntax/testing/07 coverage/02 legacy code/gilded_rose.py
@@ -2,23 +2,29 @@ from item import Item
 
 
 class GildedRose:
-    def __init__(self):
-        self.items = [
-            Item('+5 Dexterity Vest', 10, 20),
-            Item('Aged Brie', 2, 0),
-            Item('Elixir of the Mongoose', 5, 7),
-            Item('Sulfuras, Hand of Ragnaros', 0, 80),
-            Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
-            Item('Conjured Mana Cake', 3, 6),
-        ]
+    
+    def __init__(self, items):
+        self.items = items
+
+        # self.items = [
+        #     Item('+5 Dexterity Vest', 10, 20),
+        #     Item('Aged Brie', 2, 0),
+        #     Item('Elixir of the Mongoose', 5, 7),
+        #     Item('Sulfuras, Hand of Ragnaros', 0, 80),
+        #     Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+        #     Item('Conjured Mana Cake', 3, 6),
+        # ]
 
     def update_quality(self):
+
         for i in range(0, len(self.items)):
+
             if ('Aged Brie' != self.items[i].name and
                 'Backstage passes to a TAFKAL80ETC concert' != self.items[i].name):
                 if self.items[i].quality > 0:
                     if 'Sulfuras, Hand of Ragnaros' != self.items[i].name:
                         self.items[i].quality = (self.items[i].quality - 1)
+            
             else:
                 if self.items[i].quality < 50:
                     self.items[i].quality = (self.items[i].quality + 1)
