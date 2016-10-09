7960195e001bd25c08c4edeed5833a859dc51206
Royce Remulla <royce.com@gmail.com>
2016-10-09 18:45:09 +0800

    Minor fixes

--- filesystem-utils/folder_to_indexfile.py
+++ filesystem-utils/folder_to_indexfile.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3.5
 # -*- coding: utf-8 -*-
 
-
-# This script is used to dump the folder structure for content 
-# analysis.  Source Folder and Destinations are hard coded. @see `root_path` and
-# `target`
+# This script is used to dump the folder structure for content
+# analysis.  Source Folder and Destinations are hard coded. @see `root_path`
+# and `target`
 
 
 import os
@@ -18,21 +17,23 @@ logger.setLevel(logging.INFO)
 
 def ignore(path):
     return 'Working File' in path or \
-    '.DS_Store' == path or 'Exercise File' in path 
+           '.DS_Store' == path or 'Exercise File' in path
 
 
 # @param target - file handle for writing.
 # @param path - path to recurse
-def print_folder_contents(f, path, depth = 0):
+def print_folder_contents(f, path, depth=0):
     space = '' if depth == 0 else ' ' * (depth - 2)
 
     rel_path = os.path.relpath(path, root_path)
-    
+
     if rel_path != '.':
         logger.debug(space + rel_path)
         f.write(space + rel_path + '\n')
 
-    folders = [x  for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))]
+    folders = [x for x in os.listdir(path)
+               if os.path.isdir(os.path.join(path, x))]
+
     for folder in folders:
         if not ignore(folder):
             print_folder_contents(f, os.path.join(path, folder), depth + 2)
@@ -52,4 +53,3 @@ print_folder_contents(f, root_path)
 f.close
 
 print('End.')
-
