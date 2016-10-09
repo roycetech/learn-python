8820b35ccd451813cb702b6c76b06df231e2adca
Royce Remulla <royce.com@gmail.com>
2016-10-09 19:59:19 +0800

    Completed Level 3

--- learn-django/Treasuregram/Treasuregram/settings.py
+++ learn-django/Treasuregram/Treasuregram/settings.py
@@ -88,16 +88,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        'NumericPasswordValidator',
     },
 ]
 
@@ -120,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- learn-django/Treasuregram/main_app/urls.py
+++ learn-django/Treasuregram/main_app/urls.py
@@ -1,6 +1,17 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.views.static import serve
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.index)
+    url(r'^$', views.index, name='index'),
+    url(r'^([0-9]+)/$', views.detail, name='detail'),
+    url(r'^post_url/$', views.detail, name='detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {'document_root':
+            settings.MEDIA_ROOT})
+    ]
