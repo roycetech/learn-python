4549608170efbfb77b1be71646b04c80691c60a1
Royce Remulla <royce.com@gmail.com>
2016-10-09 09:05:57 +0800

    Completed Try Django

--- learn-django/Treasuregram/main_app/views.py
+++ learn-django/Treasuregram/main_app/views.py
@@ -1,7 +1,29 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Treasure
+# from django.http import HttpResponse
+
 
 # Create your views here.
-  
 def index(request):
-    return HttpResponse('<h1>Hello Explorers!</h1>')
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasures': treasures})
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+# class Treasure:
+#     def __init__(self, name, value, material, location, img_url):
+#         self.name = name
+#         self.value = value
+#         self.material = material
+#         self.location = location
+#         self.img_url = img_url
+
+
+# treasures = [
+#     t = Treasure('Gold Nugget', 500.00, 'Gold', "Curly's Creek, NM", 'http://www.example.com/gold.png'),
+#     Treasure("Fool's Gold", 0, 'Pyrite', "Fool's Falls, CO", 'http://www.example.com/gold.png'),
+#     Treasure('Coffee Can', 20.00, 'Aluminum', 'Acme, CA', 'http://www.example.com/gold.png')
+# ]
