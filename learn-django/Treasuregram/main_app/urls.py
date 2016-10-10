71a1f7d9fccba376b3c1564f30bbf7a215255955
Royce Remulla <royce.com@gmail.com>
2016-10-10 22:06:00 +0800

    Completed Lv 4, Sec 2.

--- learn-django/Treasuregram/main_app/urls.py
+++ learn-django/Treasuregram/main_app/urls.py
@@ -5,9 +5,11 @@ from . import views
 
 
 urlpatterns = [
+    url(r'^user/(\w+)/$', views.profile, name='profile'),
     url(r'^$', views.index, name='index'),
     url(r'^([0-9]+)/$', views.detail, name='detail'),
-    url(r'^post_url/$', views.detail, name='detail'),
+    url(r'^post_url/$', views.post_treasure, name='post_treasure'),
+    url(r'^login/$', views.login_view, name='login'),
 ]
 
 if settings.DEBUG:
--- learn-django/Treasuregram/main_app/views.py
+++ learn-django/Treasuregram/main_app/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from .models import Treasure
-from .forms import TreasureForm
+from .forms import TreasureForm, LoginForm
 from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate, login
+# , logout
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username=u, password=p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print("The account has been disabled!")
+            else:
+                print("The username and password were incorrect.")
+        else:
+            pass
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
 
 
 # Create your views here.
@@ -24,10 +50,17 @@ def home(request):
 def post_treasure(request):
     form = TreasureForm(request.POST, request.FILES)
     if form.is_valid():
-        form.save(commit=True)
-
+        treasure = form.save(commit=False)
+        treasure.user = request.user
+        treasure.save()
     return HttpResponseRedirect('/')
 
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    treasures = Treasure.objects.filter(user=user)
+    return render(request, 'profile.html', {'username': username,
+                                            'treasures': treasures})
 
 # class Treasure:
 #     def __init__(self, name, value, material, location, img_url):
